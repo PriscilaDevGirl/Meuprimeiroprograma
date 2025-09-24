18b41041d0832b5ce2c01f46cb56aeee37d3c527
Priscila Almeida <pryu276@gmail.com>
2025-09-23 21:28:44 -0300

    Add files via upload

---
+++ Programa de Saudação.py
@@ -0,0 +1,29 @@
+# 1 - Programa de Saudação
+print("\n--- Programa de Saudação ---")
+print("Olá, mundo!\n")
+
+# 2 - Calculadora de Soma
+print("\n--- Calculadora de Soma ---")
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+print(f"A soma de {numero1} e {numero2} é: {soma}\n")
+
+# 3 - Calculadora de Volume
+print("\n--- Calculadora de Volume ---")
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento * largura * altura
+print(f"O volume da caixa é: {volume} cm³\n")
+
+# 4 - Calculadora de Preço Total
+print("\n--- Calculadora de Preço Total ---")
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+preco_total = preco_unitario * quantidade
+print(f"Produto: {nome_produto}")
+print(f"Preço unitário: R$ {preco_unitario}")
+print(f"Quantidade: {quantidade}")
+print(f"Preço total: R$ {preco_total}\n")
